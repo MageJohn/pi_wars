2ba12499d46f3f7c7eb040c4acd9fdbbfaac4002
Yuri Pieters <magejohnyjtp@gmail.com>
2020-01-22 12:09:41 +0000

    Fix some import issues

--- examples/motorInterfaceTest.py
+++ examples/motorInterfaceTest.py
@@ -1,18 +1,18 @@
-from evdev import InputDevice, list_devices
+from evdev import InputDevice, list_devices, ecodes
 from smbus2 import SMBus
 
 from pi_wars import Controller, Stick, drive_from_vector
-from yrk_oo.motors import MOTOR1, MOTOR2, MOTOR3, MOTOR4, MotorDriver, ecodes
+from yrk_oo.motors import Motors, MotorDriver
 
 if __name__ == "__main__":
     device = InputDevice(list_devices()[0])
     stick = Stick(ecodes.ABS_X, ecodes.ABS_Y, deadzone=0.1)
     controller = Controller(device, [stick])
     bus = SMBus(13)
-    m1 = MotorDriver(MOTOR1, bus)
-    m2 = MotorDriver(MOTOR4, bus)
-    m11 = MotorDriver(MOTOR2, bus)
-    m22 = MotorDriver(MOTOR3, bus)
+    m1 = MotorDriver(Motors.MOTOR1, bus)
+    m2 = MotorDriver(Motors.MOTOR4, bus)
+    m11 = MotorDriver(Motors.MOTOR2, bus)
+    m22 = MotorDriver(Motors.MOTOR3, bus)
 
     for _ in controller.read_loop():
         m1_v, m2_v = drive_from_vector(*stick.value)
