1276684e2d6828d39a875408d8a8f568f2582b7f
Yuri Pieters <magejohnyjtp@gmail.com>
2020-01-22 12:01:51 +0000

    Finish making project into a python package

--- src/pi_wars/__init__.py
+++ src/pi_wars/__init__.py
@@ -1,2 +1,2 @@
-from controller import Stick, Controller
-from drive_from_vector import drive_from_vector
+from .controller import Stick, Controller
+from .drive_from_vector import drive_from_vector
